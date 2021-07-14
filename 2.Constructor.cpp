290ccf25586cbc5a7fe95a919c42d5d061e95c38
tapiwa <h200403@hit.ac.zw>
2021-07-14 19:36:58 +0200

    Constructor

---
+++ 2.Constructor.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+class student
+{
+public:
+    student()
+    { //This is a default constructor it is evokved at the runtime of object creation
+        //This is known as a defualt constructor
+        cout << "This is a default constructor" << endl;
+    }
+};
+class teacher
+{
+public:
+    int id;
+    string name;
+    string subject;
+    teacher(int a, string b, string c) //This is known as a parameterized constructor
+    {
+        id = a;
+        name = b;
+        subject = c;
+    }
+    void display()
+    {
+        cout << "The id is " << id << endl;
+        cout << "The name is " << name << endl;
+        cout << "The Subject is " << subject << endl;
+    }
+};
+
+int main(int argc, char const *argv[])
+{
+    student s1; //creating an object
+    student s2; // creating an object
+
+    teacher t1 = teacher(123, "Tapiwa", "History"); //creating an object
+    t1.display();
+    return 0;
+}
